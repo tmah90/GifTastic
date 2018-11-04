bcd1cd4023bcbdf036812cecf5cebb6705aeb554
Tracy Mah <tracy.y.mah@gmail.com>
2018-11-04 01:21:03 -0400

    Gif not showing up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,11 +5,10 @@ function createButtons(){
     $('#buttonOriginal').empty(); //avoiding duplicates 
 //looping thru shows 
 for (var i=0; i<disney.length; i++){
-    var button=$('<button>');
+    var button= $('<button>');
     button.html(disney[i]);
     button.addClass('btn btn-outline-secondary');
-    button.attr('data-name',disney[i]);
-    button.text(disney[i]);
+    button.attr('data-disney',disney[i]);
     $("buttonOriginal").append(button);
     }
 }
@@ -19,20 +18,24 @@ $('#addDisney').on('click',function(event){
   var DisneyNew =$("#disney-Input").val().trim();
   disney.push(DisneyNew);
   createButtons();
+  return false;
 });
 //display gifs
 function showGifs(){
-    var disney=$(this).attr("data-name");
+    var disney=$(this).attr("data-disney");
     //console.log(disney); it worked!
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + disney + "&api_key=VMdp7LyrDEY0uYvsr3Wth3EJBynw4fw7";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        disney + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + disney + "&api_key=VMdp7LyrDEY0uYvsr3Wth3EJBynw4fw7&limit=10&tag=disney";
 //AJAX GET 
 $.ajax({
     url: queryURL,
     method: "GET"
   })
-  .done(function(response){
+  .then(function(response){
     //   console.log(response.data);
-      var results= response.data;
+      var results= response.data.image_original_url;
       for(var i=0; i<results.length; i++){
     var gifDiv =$('<div class=gifs>');
     var gifImgs =$('<img>');
@@ -49,8 +52,7 @@ $.ajax({
 
     // function for displaying show gifs
     $(document).on("click", ".disney", showGifs);
-
-      }
   }
-  )
+  });
+  createButtons();
 }
